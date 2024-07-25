d4341c982818794a3ea365653a3bdc084492c77e
Diogo Silva <59505963+diogoos@users.noreply.github.com>
2024-07-25 13:06:33 -0300

    add tests to lexer

--- src/lexer.rs
+++ src/lexer.rs
@@ -73,8 +73,8 @@ impl Lexer {
 
         let max_length = self.buffer.len();
         loop {
-            if self.index >= max_length { break };
-            let c = self.buffer.chars().nth(self.index).expect("Empty char!");
+            if self.index == max_length { self.index += 1; break };
+            let c = self.buffer.chars().nth(self.index).expect("Lexer advanced past EOF!");
             
             match state {
                 S::Start => match c {
@@ -128,8 +128,8 @@ impl Lexer {
                     // we encountered an invalid token -- return it directly
                     _ => {
                         result.tag = T::Invalid;
-                        result.range.end = self.index;
                         self.index += 1;
+                        result.range.end = self.index;
                         return result;
                     }
                 },
@@ -159,7 +159,62 @@ impl Lexer {
             self.index += 1;
         }
 
-        result.range.end = self.index;
+        if matches!(result.tag, T::Eof) {
+            result.range.end = result.range.start;
+        } else {
+            result.range.end = self.index;
+        }
+
         return result;
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_return_2() {
+        let test_str = "int main(void) {\n  return 2;\n}\n";
+        let mut lexer = Lexer { buffer: test_str.to_string(), index: 0 };
+
+        assert_eq!(lexer.next(), Token { tag: Tag::KInt, range: 0..3 });
+        assert_eq!(lexer.next(), Token { tag: Tag::Identifier, range: 4..8 });
+        assert_eq!(lexer.next(), Token { tag: Tag::LParen, range: 8..9 });
+        assert_eq!(lexer.next(), Token { tag: Tag::KVoid, range: 9..13 });
+        assert_eq!(lexer.next(), Token { tag: Tag::RParen, range: 13..14 });
+        assert_eq!(lexer.next(), Token { tag: Tag::LBrace, range: 15..16 });
+        assert_eq!(lexer.next(), Token { tag: Tag::KReturn, range: 19..25 });
+        assert_eq!(lexer.next(), Token { tag: Tag::NumberLiteral, range: 26..27 });
+        assert_eq!(lexer.next(), Token { tag: Tag::Semicolon, range: 27..28 });
+        assert_eq!(lexer.next(), Token { tag: Tag::RBrace, range: 29..30 });
+        assert_eq!(lexer.next(), Token { tag: Tag::Eof, range: 31..31 });
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_lex_out_of_bounds() {
+        let test_str = "int";
+        let mut lexer = Lexer { buffer: test_str.to_string(), index: 0 };
+        
+        assert_eq!(lexer.next(), Token { tag: Tag::KInt, range: 0..3 });
+        assert_eq!(lexer.next(), Token { tag: Tag::Eof, range: 4..4 });
+        lexer.next(); // should panic
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_empty_string() {
+        let mut lexer = Lexer { buffer: "".to_string(), index: 0 };
+        assert_eq!(lexer.next(), Token { tag: Tag::Eof, range: 0..0 });
+        lexer.next(); // should panic, for consistency
+    }
+
+    #[test]
+    fn test_invalid_char() {
+        let mut lexer = Lexer { buffer: "int @ void\0".to_string(), index: 0 };
+        assert_eq!(lexer.next(), Token { tag: Tag::KInt, range: 0..3 });
+        assert_eq!(lexer.next(), Token { tag: Tag::Invalid, range: 4..5 });
+        assert_eq!(lexer.next(), Token { tag: Tag::KVoid, range: 6..10 });
     }
 }
